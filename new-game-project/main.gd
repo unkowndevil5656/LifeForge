f4306e9e0b83b1ed105da2d6c21b259ecec64f82
unkowndevil5656 <uziabdul56@gmail.com>
2026-01-05 17:51:15 +0400

    id

---
+++ new-game-project/main.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+var life_energy := 0
+var life_per_click := 1
+var life_per_second := 0
+
+@onready var life_label := $CanvasLayer/LifeLabel
+
+func _ready():
+	update_ui()
+
+func update_ui():
+	life_label.text = "Life Energy: " + str(life_energy)
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		life_energy += life_per_click
+		update_ui()
+
+func _on_Timer_timeout():
+	life_energy += life_per_second
+	update_ui()
+
+func _on_UpgradeButton_pressed():
+	if life_energy >= 10:
+		life_energy -= 10
+		life_per_second += 1
+		update_ui()
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	pass # Replace with function body.
